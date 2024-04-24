477323adf25aea7f9395c5ecec25ea612c5150bc
ITweeno <itweeno@gmail.com>
2024-04-24 14:35:47 +0100

    add iframe resizer

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "iframe-resizer/js/iframeResizer.contentWindow.js";
 import "./index.css";
 import App from "./App";
 import { DeskproAppProvider } from "@deskpro/app-sdk";
