b1a59545ca3413c08b5bc74cbee51c26ab96be42
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-01-28 14:19:50 +0000

    update VerifySettings and VerifySettingsPage to initialise settings with default values

--- src/pages/VerifySettingsPage/VerifySettingsPage.tsx
+++ src/pages/VerifySettingsPage/VerifySettingsPage.tsx
@@ -21,9 +21,16 @@ const getError = (err: Error): string => {
 };
 
 const VerifySettingsPage: FC = () => {
+  const initialSettings: Settings = {
+    instance_url: '',
+    access_token: '',
+    default_comment_on_ticket_note: true,
+    default_comment_on_ticket_reply: true
+  };
+
   const { client } = useDeskproAppClient();
   const [accounts, setAccounts] = useState<Array<IAccount["name"]>>([]);
-  const [settings, setSettings] = useState<Settings>({});
+  const [settings, setSettings] = useState<Settings>(initialSettings);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string|null>(null);
 
