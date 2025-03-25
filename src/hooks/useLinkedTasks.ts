35d0ae66824c3555ee948d60abfe9eb52e3c023f
OJ Abba <phavorabba@gmail.com>
2025-03-25 10:16:02 +0000

    [Bug] Fix API call in `useLinkedTasks` causing app to crash

--- src/hooks/useLinkedTasks.ts
+++ src/hooks/useLinkedTasks.ts
@@ -7,7 +7,7 @@ import { getEntityListService } from "@/services/deskpro";
 import { getTasksByIds } from "@/services/wrike";
 import { QueryKey } from "@/utils/query";
 import type { DPTicket, Settings } from "@/types";
-import type { ITaskFromList } from "@/services/wrike/types";
+import type { ITaskFromList, IWrikeResponse } from "@/services/wrike/types";
 
 type UseLinkedTasks = () => {
   isLoading: boolean;
@@ -15,7 +15,7 @@ type UseLinkedTasks = () => {
 };
 
 const useLinkedTasks: UseLinkedTasks = () => {
-  const { context } = useDeskproLatestAppContext<{ticket: {id: string}}, Settings>();
+  const { context } = useDeskproLatestAppContext<{ ticket: { id: string } }, Settings>();
   const location = useLocation();
   const ticketId = context?.data?.ticket.id;
 
@@ -29,9 +29,14 @@ const useLinkedTasks: UseLinkedTasks = () => {
 
   const taskIds = linkedIds.data || [];
 
-  const tasks = useQueryWithClient(
+  const tasks = useQueryWithClient<IWrikeResponse<ITaskFromList[]>>(
     [QueryKey.TASKS, ...taskIds],
-    (client) => getTasksByIds(client, taskIds, context?.settings),
+    (client) => {
+      return getTasksByIds(client, taskIds, context?.settings).catch(() => ({
+        kind: "",
+        data: []
+      }));
+    },
     { enabled: taskIds.length > 0 && Boolean(context?.settings) && !isOnLogInPage }
   );
 
