454c85bd5745d8efde3104b496ed99057bb87663
OJ Abba <phavorabba@gmail.com>
2025-03-27 12:29:34 +0000

    [Refactor] Add error handling for access tokens to the "loading" page

--- src/pages/LoadingAppPage/LoadingAppPage.tsx
+++ src/pages/LoadingAppPage/LoadingAppPage.tsx
@@ -1,15 +1,79 @@
-import { LoadingSpinner } from "@deskpro/app-sdk";
-import { useSetTitle } from "@/hooks";
-import { useLoadingApp } from "./hooks";
-import type { FC } from "react";
+import { checkAuthService } from "@/services/wrike";
+import { ErrorBlock } from "@/components/common";
+import { getEntityListService } from "@/services/deskpro";
+import { LoadingSpinner, useDeskproAppClient, useDeskproElements, useDeskproLatestAppContext, useInitialisedDeskproAppClient } from "@deskpro/app-sdk";
+import { LOG_IN_TYPE_STATE, logInTypes } from "@/constants";
+import { Settings, TicketData } from "@/types";
+import { useNavigate } from "react-router-dom";
+import { useState, type FC } from "react";
 
 const LoadingAppPage: FC = () => {
-  useLoadingApp();
+  const { client } = useDeskproAppClient()
+  const { context } = useDeskproLatestAppContext<TicketData, Settings>()
 
-  useSetTitle();
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
+  const [isFetchingAuth, setIsFetchingAuth] = useState<boolean>(true)
+
+
+  const navigate = useNavigate()
+
+  // Determine authentication method from settings
+  const isUsingOAuth = context?.settings.use_access_token !== true || context.settings.use_advanced_connect === false
+  const ticketId = context?.data?.ticket.id
+
+  useDeskproElements(({ registerElement, clearElements }) => {
+    clearElements()
+    registerElement("refresh", { type: "refresh_button" })
+  })
+
+  useInitialisedDeskproAppClient((client) => {
+    client.setTitle("Wrike")
+
+    if (!context || !context?.settings || !ticketId) {
+      return
+    }
+
+    // Store the authentication method in the user state
+    client.setUserState(LOG_IN_TYPE_STATE, isUsingOAuth ? logInTypes.OAUTH2 : logInTypes.ACCESS_TOKEN);
+
+    // Verify authentication status
+    // If OAuth2 mode and the user is logged in the request would be make with their stored access token
+    // If in access token mode the request would be made with the access token provided in the app setup
+    checkAuthService(client, context.settings)
+      .then((user) => {
+        if (user) {
+          setIsAuthenticated(true)
+        }
+      })
+      .catch(() => { })
+      .finally(() => {
+        setIsFetchingAuth(false)
+      })
+  }, [context, context?.settings])
+
+
+  if (!client || !ticketId || isFetchingAuth) {
+    return (<LoadingSpinner />)
+  }
+
+  if (isAuthenticated) {
+    // Redirect the user based on their amount of linked tasks
+    getEntityListService(client, ticketId)
+      .then((entityIds) => navigate(entityIds.length > 0 ? "/home" : "/tasks/link"))
+      .catch(() => { navigate("/tasks/link") });
+  } else if (isUsingOAuth) {
+    navigate("/log_in");
+  } else {
+    // Show error for invalid access tokens (expired or not present)
+    return (
+      <div style={{padding: 12}}>
+        <ErrorBlock texts={["Invalid Access Token"]} />
+      </div>
+    );
+  }
 
   return (
-    <LoadingSpinner/>
+    <LoadingSpinner />
   );
 };
 
