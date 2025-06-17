c2a45f7dfc0f8da44be1c3b3a8331be612a37808
OJ Abba <phavorabba@gmail.com>
2025-06-17 09:45:49 +0100

    [Chore] Update `ErrorFallback` typing

--- src/components/ErrorFallback/ErrorFallback.tsx
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -3,25 +3,28 @@ import { AnyIcon, Button, H1, H2, Stack } from "@deskpro/deskpro-ui";
 import { parseJsonErrorMessage } from "@/utils";
 import { Container } from "@/components/common";
 
-export const ErrorFallback = ({
-  error,
-  resetErrorBoundary,
-}: {
-  error: Error;
-  resetErrorBoundary: () => void;
-}) => {
+interface ErrorFallbackProps {
+  error: unknown;
+  componentStack: string;
+  eventId: string;
+  resetError(): void;
+}
+
+export function ErrorFallback(props: Readonly<ErrorFallbackProps>) {
+  const { error, resetError } = props
   return (
     <Container>
       <Stack vertical gap={10} role="alert">
         <H1>Something went wrong:</H1>
-        <H2>{parseJsonErrorMessage(error.message)}</H2>
+        {/* This needs to be updated in the future as regular errors will cause this to crash. */}
+        <H2>{error instanceof Error ? parseJsonErrorMessage(error.message) : "An unknown error occurred"}</H2>
         <Button
           text="Reload"
-          onClick={resetErrorBoundary}
+          onClick={resetError}
           icon={faRefresh as AnyIcon}
           intent="secondary"
-          />
+        />
       </Stack>
     </Container>
-  );
-};
+  )
+}
